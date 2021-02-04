fa9c23f66841d8f30aff610a95ca26ca55293faa
Georgi Dimulski <george.dimulski@gmail.com>
2021-02-04 16:20:24 +0200

    Stringify body and create bill

--- api/billsOfLading/createBillOfLading.js
+++ api/billsOfLading/createBillOfLading.js
@@ -8,6 +8,7 @@ export default async (req, res) => {
     const dateRegex = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
     const { body } = req;
     const parsedBody = JSON.parse(body);
+
     // TODO: Basic validation, find a framework that does this properly
     const isBillValid =
       parsedBody.description.length >= 5 &&
@@ -27,20 +28,18 @@ export default async (req, res) => {
       parsedBody.receiverAddress.length <= 200;
 
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-    res.json(isBillValid);
-    return;
-    // if (isBillValid) {
-    //   client
-    //     .query(Create(Collection("bills_of_lading"), { data: body }))
-    //     .then((newBillResponse) => {
-    //       const bill = newBillResponse.data;
-    //       bill["ref"] = newBillResponse.ref.id;
-    //       res.json(bill);
-    //     });
-    // } else {
-    //   res.json("Incorrect Bill of Lading information");
-    // }
+    if (isBillValid) {
+      client
+        .query(Create(Collection("bills_of_lading"), { data: body }))
+        .then((newBillResponse) => {
+          const bill = newBillResponse.data;
+          bill["ref"] = newBillResponse.ref.id;
+          res.json(bill);
+        });
+    } else {
+      res.json("Incorrect Bill of Lading information");
+    }
   } catch (error) {
-    res.status(501).json({ error });
+    res.status(500).json({ error });
   }
 };
