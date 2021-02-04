d803d705ac6c54142d84dfea6991e0eae8885ca9
Georgi Dimulski <george.dimulski@gmail.com>
2021-02-04 14:56:04 +0200

    Return createBill body

--- api/billsOfLading/createBillOfLading.js
+++ api/billsOfLading/createBillOfLading.js
@@ -7,28 +7,27 @@ export default async (req, res) => {
   try {
     const dateRegex = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
     const { body } = req;
-
-    // TODO: Basic validation, find a framework that does this properly
-    const isBillValid =
-      body.description.length >= 5 &&
-      body.description.length <= 200 &&
-      body.weight >= 0.1 &&
-      body.weight <= 80 &&
-      body.packages >= 1 &&
-      body.packages <= 10 &&
-      body.senderName.length >= 5 &&
-      body.senderName.length <= 50 &&
-      body.senderAddress.length >= 5 &&
-      body.senderAddress.length <= 200 &&
-      dateRegex.test(body.deliveryDate) &&
-      body.receiverName.length >= 5 &&
-      body.receiverName.length <= 50 &&
-      body.receiverAddress.length >= 5 &&
-      body.receiverAddress.length <= 200;
-
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-    res.json("isBillValid " + isBillValid);
-    return;
+    res.json(body);
+    // TODO: Basic validation, find a framework that does this properly
+    // const isBillValid =
+    //   body.description.length >= 5 &&
+    //   body.description.length <= 200 &&
+    //   body.weight >= 0.1 &&
+    //   body.weight <= 80 &&
+    //   body.packages >= 1 &&
+    //   body.packages <= 10 &&
+    //   body.senderName.length >= 5 &&
+    //   body.senderName.length <= 50 &&
+    //   body.senderAddress.length >= 5 &&
+    //   body.senderAddress.length <= 200 &&
+    //   dateRegex.test(body.deliveryDate) &&
+    //   body.receiverName.length >= 5 &&
+    //   body.receiverName.length <= 50 &&
+    //   body.receiverAddress.length >= 5 &&
+    //   body.receiverAddress.length <= 200;
+
+    // return;
     // if (isBillValid) {
     //   client
     //     .query(Create(Collection("bills_of_lading"), { data: body }))
