5ae6f4aa1e341d4915abb8bca08b849bcc542f55
George Dimulski <g.dimulski@codecoda.com>
2021-02-04 09:52:00 +0200

    Add basic validation to createBillOfLading endpoint

--- api/billsOfLading/createBillOfLading.js
+++ api/billsOfLading/createBillOfLading.js
@@ -5,17 +5,38 @@ const { Create, Collection } = faunadb.query;
 
 export default async (req, res) => {
   try {
-    const bill = req.body;
+    const dateRegex = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
     const { body } = req;
-    // TODO: validate request
-    // if (bill.description.length)
 
-    const newBillOfLading = client.query(
-      Create(Collection("bills_of_lading"), { data: body })
-    );
+    // TODO: Basic validation, find a framework that does this properly
+    const isBillValid =
+      body.description.length >= 5 &&
+      body.description.length <= 200 &&
+      body.weight >= 0.1 &&
+      body.weight <= 80 &&
+      body.packages >= 1 &&
+      body.packages <= 10 &&
+      body.senderName.length >= 5 &&
+      body.senderName.length <= 50 &&
+      body.senderAddress.length >= 5 &&
+      body.senderAddress.length <= 200 &&
+      dateRegex.test(body.deliveryDate) &&
+      body.receiverName.length >= 5 &&
+      body.receiverName.length <= 50 &&
+      body.receiverAddress.length >= 5 &&
+      body.receiverAddress.length <= 200;
 
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-    res.json(body);
+
+    if (isBillValid) {
+      const newBillOfLading = client.query(
+        Create(Collection("bills_of_lading"), { data: body })
+      );
+
+      res.json(newBillOfLading);
+    } else {
+      res.json("Incorrect Bill of Lading information");
+    }
   } catch (error) {
     res.status(500).json({ error });
   }
